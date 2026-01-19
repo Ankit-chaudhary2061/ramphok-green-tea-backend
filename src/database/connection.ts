c67692960a8a9299d5315c9a4e97a6d9db9e6970
Ankit Chaudhary <ankitchau2061@gmail.com>
2026-01-19 10:13:10 -0800

    backend setup

---
+++ src/database/connection.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { Dialect } from "sequelize";
+dotenv.config();
+import { Sequelize } from "sequelize-typescript";
+const dialect: Dialect = (process.env.DB_DIALECT as Dialect) || "mysql";
+
+
+
+const sequelize  = new Sequelize({
+     database: process.env.DB_NAME || "rampokha",
+  username: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 3306,
+  dialect,
+})
+
+
+export default sequelize
